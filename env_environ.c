501d160c6b12be0996a0e09bca43eda49e44ee79
Fredah14 <fredah.jr@hotmail.com>
2023-07-27 21:26:25 -0700

    env_environ.c

--- env_environ.c
+++ env_environ.c
@@ -1,12 +1,12 @@
 #include "shell.h"
 
 /**
- * _myenv - prints the current environment
+ * _env - prints the current environment
  * @info: contains potential arguments to main constant fun prototype.
  *
  * Return: Always 0
  */
-int _myenv(info_t *info)
+int _env(info_t *info)
 {
 	print_list_str(info->env);
 	return (0);
