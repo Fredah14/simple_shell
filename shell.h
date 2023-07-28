b7cf1f221e10ae14e0c3758ff2552e156b322fa2
Fredah14 <fredah.jr@hotmail.com>
2023-07-28 04:54:55 -0700

    shell.h

--- shell.h
+++ shell.h
@@ -187,7 +187,7 @@ ssize_t get_nodes_index(list_t *, list_t *);
 int is_chain(info_t *, char *, size_t *);
 void check_chain(info_t *, char *, size_t *, size_t, size_t);
 int replace_alias(info_t *);
-int replace_vars(info_t *);
+int replace_variables(info_t *);
 int replace_string(char **, char *);
 
 #endif
