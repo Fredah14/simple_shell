e48337aa9d55f34f6cfdbb34ac9cc5890e90b0b5
Fredah14 <fredah.jr@hotmail.com>
2023-07-28 05:13:46 -0700

    _errors.c

--- _errors.c
+++ _errors.c
@@ -1,49 +1,51 @@
+#include <stdio.h>
+#include <unistd.h>
 #include "shell.h"
 
 /**
- *_errputs - prints an input string
+ *_errorputs - prints an input string
  * @str: string to be printed
  *
  * Return: 0 Always
  */
-void _errputs(char *str)
+void _errorputs(char *str)
 {
-	int i = 0;
+	int j = 0;
 
 	if (!str)
 		return;
-	while (str[i] != '\0')
+	while (str[j] != '\0')
 	{
-		_eputchar(str[i]);
-		i++;
+		_eputchar(str[j]);
+		j++;
 	}
 }
 
 /**
- * _eputchar - writes the character c to stderr
- * @c: character to print
+ * _eputchar - writes the character to stderr
+ * @c: character address to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _eputchar(char c)
 {
-	static int i;
+	static int j;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (c == BUF_FLUSH || j >= WRITE_BUF_SIZE)
 	{
-		write(2, buf, i);
-		i = 0;
+		write(2, buf, j);
+		j = 0;
 	}
 	if (c != BUF_FLUSH)
-		buf[i++] = c;
+		buf[j++] = c;
 	return (1);
 }
 
 /**
- * _putfd - writes the character c to given filedescriptor
- * @c: character to print
+ * _putfd - writes the character to given filedescriptor
+ * @c: character address to print
  * @fd: filedescriptor to write to
  *
  * Return: On success 1.
@@ -51,16 +53,16 @@ int _eputchar(char c)
  */
 int _putfd(char c, int fd)
 {
-	static int i;
+	static int j;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (c == BUF_FLUSH || j >= WRITE_BUF_SIZE)
 	{
-		write(fd, buf, i);
-		i = 0;
+		write(fd, buf, j);
+		j = 0;
 	}
 	if (c != BUF_FLUSH)
-		buf[i++] = c;
+		buf[j++] = c;
 	return (1);
 }
 
@@ -73,13 +75,13 @@ int _putfd(char c, int fd)
  */
 int _putsfd(char *str, int fd)
 {
-	int i = 0;
+	int j = 0;
 
 	if (!str)
 		return (0);
 	while (*str)
 	{
-		i += _putfd(*str++, fd);
+		j += _putfd(*str++, fd);
 	}
-	return (i);
+	return (j);
 }
